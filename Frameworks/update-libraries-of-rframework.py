0686c395b051ac1448421fd3f70fa693856ba542
FransMeerhoff <F.A.M.Meerhoff@uva.nl>
2018-12-20 09:38:56 +0100

    Update python script changing lib paths

--- Frameworks/update-libraries-of-rframework.py
+++ Frameworks/update-libraries-of-rframework.py
@@ -10,92 +10,93 @@ current = "3.4"
 if len(sys.argv) > 1 :
 	current = sys.argv[1]
 
-linkDir = "@executable_path/../jasp-required-files"
+linkDir = "@executable_path/.."
 if len(sys.argv) > 2 :
 	linkDir = "@executable_path/" + sys.argv[2]
 
-print "Current R Version: " + current + ", Replace links of libs with: " + linkDir 
+print "Current R Version: " + current + ", Replace links of libs with: " + linkDir
 
 def locate_libs(path):
-	
+
 
 	locations = set()
 
 	if os.path.isfile(path):
-	
+
 		if path.endswith(".so") or path.endswith(".dylib"):
-	
+
 			locations.add(path)
-			
+			print("location: "  + path)
+'''
 			output = check_output(["otool", "-L", path])
-	
+
 			lines = output.split("\n")
 			lines.pop(0)
-	
+
 			for line in lines:
-	
+
 				line = line.strip()
 
 				if line.startswith("/Library/Frameworks/R.framework/Versions/" + current + "/Resources/") or line.startswith("/opt/") or line.startswith("/usr/local/"):
-		
+
 					file = line.split()[0]
 					if os.path.isfile(file):
 						locations.add(file)
-						
+
 					if (file != path):
 						#print("recursing into " + file)
 						locations.update(locate_libs(file))
-		
+	'''
 	elif os.path.isdir(path):
-	
+
 
 		if path.endswith(".dSYM"):
 			shutil.rmtree(path)
-			#print("removing : " + path)
-			
+			print("removing : " + path)
+
 		elif path.endswith("/html") or path.endswith("/help") or path.endswith("/demo") or path.endswith("/tests"):
 			shutil.rmtree(path)
-			#print("removing : " + path)
+			print("removing : " + path)
 
 		else:
 
 			for sub in os.listdir(path):
-		
+
 				sub_path = os.path.join(path, sub)
 				locations.update(locate_libs(sub_path))
-			
+
 	asArray = [ ]
 	asArray.extend(locations)
-			
+
 	return asArray
 
 def extract_lib_dependencies(libs):
 
 	dependencies = set()
-	
+
 	for lib in libs:
-	
+
 		#print(lib)
 
 		output = check_output(["otool", "-L", lib])
-	
+
 		lines = output.split("\n")
 		lines.pop(0)
-	
+
 		for line in lines:
-	
+
 			line = line.strip()
 
 			if line.startswith("/Library/Frameworks/R.framework/") or line.startswith("/opt/") or line.startswith("/usr/local/"):
-		
+
 				file = line.split()[0]
 				dependencies.add(file)
-				
+
 	asArray = []
 	asArray.extend(dependencies)
-			
+
 	return asArray
-	
+
 def change_dep_paths(lib, changes):
 
 	for change in changes:
@@ -109,11 +110,11 @@ wd = os.getcwd()
 #shutil.copytree("/Library/Frameworks/R.framework/", os.path.join(wd, "R.framework"), symlinks=True)
 
 path = os.path.join(wd, "R.framework")
-libpath = os.path.join(wd, "R.framework/Versions/" + current + "/Resources/lib")
+librarypath = os.path.join(wd, "R.framework/Versions/" + current + "/Resources/library")
 out_lib_dir = os.path.join(wd, "R.framework/Versions/" + current + "/Resources/lib")
 
-libs = locate_libs(path)
-dependencies = [] #extract_lib_dependencies(libs)
+libs = locate_libs(librarypath)
+dependencies = extract_lib_dependencies(libs)
 
 new_libs = [ ]
 changes = [ ]
@@ -124,25 +125,25 @@ for dependency in dependencies:
 	dep_target = os.path.join(out_lib_dir, dep_base)
 
 	if os.path.isfile(dependency):
-	
+
 #		if dependency != "/opt/X11/lib/libfreetype.6.dylib":
 		shutil.copyfile(dependency, dep_target)
-			
-		new_libs.append(dep_target)
-		
+
+#		new_libs.append(dep_target)
+
 		change = { "old" : dependency, "new" : linkDir + "/Frameworks/R.framework/Versions/" + current + "/Resources/lib/" + dep_base }
 		changes.append(change)
 
 		change = { "old" : dep_base, "new" : linkDir + "/Frameworks/R.framework/Versions/" + current + "/Resources/lib/" + dep_base }
 		changes.append(change)
-		
+
 	else:
-	
+
 		print(dependency + " not found!")
 
 #print(dependencies)
 #print(changes)
-		
+
 for new_lib in new_libs:
 
 	lib_base = os.path.basename(new_lib)
@@ -151,12 +152,12 @@ for new_lib in new_libs:
 	call(["install_name_tool", "-id", new_path, new_lib])
 
 	change_dep_paths(new_lib, changes)
-	
+
 for lib in libs:
 
 	lib_base = os.path.basename(lib)
 	new_path = os.path.relpath(lib, path)
-	
+
 	new_path = new_path.replace("R.framework/Resources/", "R.framework/Versions/" + current + "/Resources/")
 	new_path = new_path.replace("R.framework/Versions/Current/", "R.framework/Versions/" + current + "/")
 	new_path = new_path.replace("R.framework/Libraries/", "R.framework/Versions/" + current + "/lib/")
@@ -164,14 +165,14 @@ for lib in libs:
 	if new_path.startswith(".."):
 		new_path = linkDir + "/Frameworks/R.framework/Versions/" + current + "/Resources/lib/" + lib_base
 	else:
-	
+
 		if new_path.startswith("Resources/"):
 			new_path = new_path.replace("Resources/", "Versions/" + current + "/Resources/")
 		if new_path.startswith("Versions/Current/"):
 			new_path = new_path.replace("Versions/Current/", "Versions/" + current + "/")
 		if new_path.startswith("Libraries/"):
 			new_path = new_path.replace("Libraries/", "Versions/" + current + "/Resources/")
-	
+
 		new_path = linkDir + "/Frameworks/R.framework/" + new_path
 
 	#print(new_path)
@@ -180,7 +181,7 @@ for lib in libs:
 	call(["install_name_tool", "-id", new_path, lib])
 
 	change_dep_paths(lib, changes)
-	
+
 def removeFunc(path):
 	try:
 		os.remove(path)
@@ -211,4 +212,3 @@ removeTreeFunc(os.path.join(wd, "R.framework/Versions/" + current + "/Resources/
 
 
 call(["install_name_tool", "-id", linkDir + "/Frameworks/R.framework/Versions/" + current + "/Resources/lib/libR.dylib", "libR.dylib"])
-
