587ea80ba973c024379e89fa546b56c1bf9974ef
Joris Goosen <Joris@JorisGoosen.nl>
2019-10-16 17:30:19 +0200

    Add JAGS for Windows

---
+++ 32/JAGS/i386/bin/jags.bat
@@ -0,0 +1,6 @@
+setlocal
+set PATH=C:\Program Files\JAGS\JAGS-4.3.0\i386\bin\;%PATH%
+set LTDL_LIBRARY_PATH=C:\Program Files\JAGS\JAGS-4.3.0\i386\modules
+cls
+jags-terminal %1
+
---
+++ 64/JAGS/x64/bin/jags.bat
@@ -0,0 +1,6 @@
+setlocal
+set PATH=C:\Program Files\JAGS\JAGS-4.3.0\x64\bin\;%PATH%
+set LTDL_LIBRARY_PATH=C:\Program Files\JAGS\JAGS-4.3.0\x64\modules
+cls
+jags-terminal %1
+
