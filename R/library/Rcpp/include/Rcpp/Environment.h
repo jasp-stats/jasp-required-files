051c611e59aed1e6d0d4ff1bf6d3a3391a3928d3
FransMeerhoff <F.A.M.Meerhoff@uva.nl>
2021-01-27 13:21:35 +0100

    Update package Bayesrel v 0.7.0.3 ( and Rcpp v 1.0.6 )
    Update package Bayesrel v 0.7.0.3 ( and Rcpp v 1.0.6 )
    Request Julius 26-01-2021
    
    Source :
    https://github.com/juliuspf/Bayesrel
    remotes::install_github("juliuspf/Bayesrel")
    Update Rcpp to 1.0.6 from cran

--- R/library/Rcpp/include/Rcpp/Environment.h
+++ R/library/Rcpp/include/Rcpp/Environment.h
@@ -2,8 +2,8 @@
 //
 // Environment.h: Rcpp R/C++ interface class library -- access R environments
 //
-// Copyright (C) 2009 - 2013    Dirk Eddelbuettel and Romain Francois
-// Copyright (C) 2014           Dirk Eddelbuettel, Romain Francois and Kevin Ushey
+// Copyright (C) 2009 - 2013  Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2014 - 2020  Dirk Eddelbuettel, Romain Francois and Kevin Ushey
 //
 // This file is part of Rcpp.
 //
@@ -33,8 +33,8 @@ namespace Rcpp{
             if( Rf_isEnvironment(x) ) return x ;
             SEXP asEnvironmentSym = Rf_install("as.environment");
             try {
-                Shield<SEXP> res(Rcpp_fast_eval(Rf_lang2(asEnvironmentSym, x), R_GlobalEnv));
-                return res ;
+                Shield<SEXP> call(Rf_lang2(asEnvironmentSym, x));
+                return Rcpp_fast_eval(call, R_GlobalEnv);
             } catch( const eval_error& ex) {
                 const char* fmt = "Cannot convert object to an environment: "
                                   "[type=%s; target=ENVSXP].";
@@ -246,9 +246,8 @@ namespace Rcpp{
                        we have to go back to R to do this operation */
                     SEXP internalSym = Rf_install( ".Internal" );
                     SEXP removeSym = Rf_install( "remove" );
-                    Shield<SEXP> call( Rf_lang2(internalSym,
-                            Rf_lang4(removeSym, Rf_mkString(name.c_str()), Storage::get__(), Rf_ScalarLogical( FALSE ))
-                        ) );
+                    Shield<SEXP> str(Rf_mkString(name.c_str()));
+                    Shield<SEXP> call(Rf_lang2(internalSym, Rf_lang4(removeSym, str, Storage::get__(), Rf_ScalarLogical(FALSE))));
                     Rcpp_fast_eval( call, R_GlobalEnv ) ;
                 }
             } else{
@@ -374,7 +373,8 @@ namespace Rcpp{
             try{
                 SEXP getNamespaceSym = Rf_install("getNamespace");
                 Shield<SEXP> package_str( Rf_mkString(package.c_str()) );
-                env = Rcpp_fast_eval(Rf_lang2(getNamespaceSym, package_str), R_GlobalEnv);
+                Shield<SEXP> call( Rf_lang2(getNamespaceSym, package_str) );
+                env = Rcpp_fast_eval(call, R_GlobalEnv);
             } catch( ... ){
                 throw no_such_namespace( package  ) ;
             }
@@ -393,9 +393,9 @@ namespace Rcpp{
          */
         Environment_Impl new_child(bool hashed) const {
             SEXP newEnvSym = Rf_install("new.env");
-            return Environment_Impl(Rcpp_fast_eval(Rf_lang3(newEnvSym, Rf_ScalarLogical(hashed), Storage::get__()), R_GlobalEnv));
+            Shield<SEXP> call(Rf_lang3(newEnvSym, Rf_ScalarLogical(hashed), Storage::get__()));
+            return Environment_Impl(Rcpp_fast_eval(call, R_GlobalEnv));
         }
-
 
         void update(SEXP){}
     };
